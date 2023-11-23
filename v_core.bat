ae769716c45f5aaef61fba70db1a457a01855684
SparkCoder <abhishekdinesh3@gmail.com>
2023-11-23 15:15:26 +0530

    BG Changer virus

---
+++ v_core.bat
@@ -0,0 +1,33 @@
+@echo off
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d "%cd%/images/r_wp.png" >nul
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters >nul
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d "%cd%/images/r_wp.png" >nul
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters >nul
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d "%cd%/images/r_wp.png" >nul
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters >nul
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d "%cd%/images/r_wp.png" >nul
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters >nul
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d "%cd%/images/r_wp.png" >nul
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters >nul
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d "%cd%/images/r_wp.png" >nul
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters >nul
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d "%cd%/images/r_wp.png" >nul
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters >nul
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d "%cd%/images/r_wp.png" >nul
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters >nul
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d "%cd%/images/r_wp.png" >nul
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters >nul
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d "%cd%/images/r_wp.png" >nul
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters >nul
+
+@echo on 
